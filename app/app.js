1c19b8e8353da238d87b9eda268a227ab2eb700c
ES Sriram <srir32291@gmail.com>
2026-04-04 11:51:27 +0530

    Trigger rollback

--- app/app.js
+++ app/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 
+const VERSION = process.env.VERSION || "Version 2 - Working";
+
 app.get("/", (req, res) => {
-  res.send("Version 1 - App is Working");
+  res.send(VERSION);
 });
 
-// FAILURE TRIGGER
 app.get("/crash", (req, res) => {
-  process.exit(1); // simulate crash
+  process.exit(1);
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
