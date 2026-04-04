ac4694774ea264ba571eb94c1a082227e9d6e3a0
ES Sriram <srir32291@gmail.com>
2026-04-04 10:47:49 +0530

    Version 1 working

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("App Running Successfully 🚀");
+  res.send("Version 1 - Working");
 });
 
 // FAILURE TRIGGER
